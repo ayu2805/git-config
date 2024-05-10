ba70d05cda3a468f0ae74f9fdf15b2e806aa0338
Ayushmaan Padhi <76616749+ayu2805@users.noreply.github.com>
2024-05-10 18:27:25 +0530

    Update git-config.sh

--- git-config.sh
+++ git-config.sh
@@ -6,5 +6,5 @@ git config --global user.name "$git_name"
 git config --global user.email "$git_email"
 ssh-keygen -t ed25519 -C "$git_email"
 git config --global gpg.format ssh
-git config --global user.signingkey /home/$un/.ssh/id_ed25519.pub
+git config --global user.signingkey /home/$(whoami)/.ssh/id_ed25519.pub
 git config --global commit.gpgsign true
