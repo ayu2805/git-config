4ba0aeb37abd8384d10facef4ba7825f88825cee
Ayushmaan Padhi <76616749+ayu2805@users.noreply.github.com>
2024-10-16 00:47:19 +0530

    Update git-config.bat

--- git-config.bat
+++ git-config.bat
@@ -4,7 +4,7 @@ set /p git_email=Enter your Git email:
 
 git config --global user.name "%git_name%"
 git config --global user.email "%git_email%"
-ssh-keygen -t ed25519 -C "%git_email%"
+ssh-keygen -t ed25519
 git config --global gpg.format ssh
 git config --global user.signingkey  C:/Users/%username%/.ssh/id_ed25519.pub
 git config --global commit.gpgsign true
