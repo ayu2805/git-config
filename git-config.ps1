d7939070872b9888c60e7ada6ac909b98ba1dd99
Ayushmaan Padhi <76616749+ayu2805@users.noreply.github.com>
2024-10-16 00:47:04 +0530

    Update git-config.ps1

--- git-config.ps1
+++ git-config.ps1
@@ -3,7 +3,7 @@ $git_email = Read-Host "Enter your Git email"
 
 git config --global user.name "$git_name"
 git config --global user.email "$git_email"
-ssh-keygen -t ed25519 -C "$git_email"
+ssh-keygen -t ed25519 -C
 git config --global gpg.format ssh
 git config --global user.signingkey C:/Users/$env:USERNAME/.ssh/id_ed25519.pub
 git config --global commit.gpgsign true
